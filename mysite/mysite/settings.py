97e33aedc9e15715b21aded59ccb74a8ef536368
Sarah Eiting <sarah.eiting@gmail.com>
2014-09-07 19:22:14 -0700

    created requirements file, added to .gitignore, edited database config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangotutorial_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT' : '',
     }
 }
 
