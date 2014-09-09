c745ced492a9c7731df7450b779e44bf08f745e8
Sarah Eiting <sarah.eiting@gmail.com>
2014-09-09 14:11:32 -0700

    Added TEMPLATE_DIRS to settings.py. Edited admin site title.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,3 +86,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# TEMPLATE_DIRS is an iterable of filesystem directories to check when loading
+# Django templates; it's a search path.
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
