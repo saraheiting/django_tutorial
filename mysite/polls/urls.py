a4784a48ad113b7c1a67fd90908b1bfc399972fc
Sarah Eiting <sarah.eiting@gmail.com>
2014-09-10 15:20:46 -0700

    Removed hardcoded url in index template. Namespaced url names.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -11,4 +11,4 @@ urlpatterns = patterns('',
  	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
  	# ex: /polls/5/vote/
  	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),   
-)
+) 
