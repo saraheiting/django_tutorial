c14b8f4895ee648828c33be9b24f8eb482e38639
Sarah Eiting <sarah.eiting@gmail.com>
2014-09-11 11:13:12 -0700

    Added form element to detail.html

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
  	# ex: /polls/5/results/
  	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
  	# ex: /polls/5/vote/
- 	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),   
-) 
+ 	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+)
