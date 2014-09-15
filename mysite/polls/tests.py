d9cb1cdbe7e8ade3ea30c686f014d5afc190ec33
Sarah Eiting <sarah.eiting@gmail.com>
2014-09-15 10:55:34 -0700

    Edited class DetailView. Added two tests for detail view.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -16,8 +16,38 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text,
     							   pub_date=time)
 
+
+class QuestionMethodTests(TestCase):
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for questions whose 
+		pub_date is in the future
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_published_recently() should return False for questions whose 
+		pub_date is older than 1 day
+		"""
+		time = timezone.now() - datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertEqual(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_published_recently() should return True for questions whose 
+		pub_date is within the last day.
+
+		"""
+		time = timezone.now() - datetime.timedelta(hours=1)
+		recent_question = Question(pub_date=time)
+		self.assertEqual(recent_question.was_published_recently(), True)
+
+
 class QuestionViewTests(TestCase):
-    
     def test_index_view_with_no_questions(self):
         """
         If no questions exist, an appropriate message should be displayed.
@@ -75,32 +105,26 @@ class QuestionViewTests(TestCase):
             ['<Question: Past question 1.>', '<Question: Past question 2.>']
         )
 
-class QuestionMethodTests(TestCase):
+class QuestionIndexDetailTests(TestCase):
+	def test_detail_view_with_a_future_question(self):
+		"""
+		The detail view of a question with a pub_date in the future should 
+		return a 404 not found
+		"""
+		future_question = create_question(question_text='Future question',
+			                              days=5)
+		response = self.client.get(reverse('polls:detail',
+										    args=(future_question.id,)))
+		self.assertEqual(response.status_code, 404)
 
-	def test_was_published_recently_with_future_question(self):
+	def test_detail_view_with_a_past_question(self):
 		"""
-		was_published_recently() should return False for questions whose 
-		pub_date is in the future
+		The detail view of a question with a pub_date in the past should
+		display the question's text.
 		"""
-		time = timezone.now() + datetime.timedelta(days=30)
-		future_question = Question(pub_date=time)
-		self.assertEqual(future_question.was_published_recently(), False)
-
-	def test_was_published_recently_with_old_question(self):
-		"""
-		was_published_recently() should return False for questions whose 
-		pub_date is older than 1 day
-		"""
-		time = timezone.now() - datetime.timedelta(days=30)
-		old_question = Question(pub_date=time)
-		self.assertEqual(old_question.was_published_recently(), False)
-
-	def test_was_published_recently_with_recent_question(self):
-		"""
-		was_published_recently() should return True for questions whose 
-		pub_date is within the last day.
-		"""
-		time = timezone.now() - datetime.timedelta(hours=1)
-		recent_question = Question(pub_date=time)
-		self.assertEqual(recent_question.was_published_recently(), True)
-
+		past_question = create_question(question_text="Past question",
+										days=-5)
+		response = self.client.get(reverse('polls:detail',
+								   args=(past_question.id,)))
+		self.assertContains(response, past_question.question_text,
+							status_code=200)
